48c15436304dfb18824aa87abfa89b9fbef016c5
casinbott <sahilkhankklp@gmail.com>
2024-11-20 14:07:16 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,26 +10,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
+global.ownernomer = "18542254973"
+global.ownername = "Casino X Cracker"
+global.ytname = "YT: Coding Hu "
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '18542254973'  //creator number
+global.ownername = 'Casino X Cracker' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'GlobalTech\n\nContact: +18542254973'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://whatsapp.com/channel/0029VasWpZ8EFeXcSc2yQf1M'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
